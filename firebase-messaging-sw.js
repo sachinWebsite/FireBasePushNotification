768cc47780e969e3773b78d9bbbd4b59a6ce052b
sachinWebsite <104052410+sachinWebsite@users.noreply.github.com>
2022-04-20 11:59:16 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,10 @@
-importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js");
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+    "https://www.gstatic.com/firebasejs/9.0.1/firebase-messaging.js",
 );
 // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
 importScripts(
-    "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+    "https://www.gstatic.com/firebasejs/9.0.1/firebase-analytics.js",
 );
 
 const firebaseConfig = {
